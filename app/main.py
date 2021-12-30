4414c3457b90c1583ab71cc0074cbffcf2e05d6c
Patrick Carlberg <11623836+CJRockball@users.noreply.github.com>
2021-12-30 14:42:27 +0800

    import data

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+import pathlib
+
 import pandas as pd
 
-print("hello world")
+PROJECT_PATH = pathlib.Path(__file__).resolve().parent.parent
+SOURCE_DATA_FILE = PROJECT_PATH / "data/source_data/wfa_girls_0-to-13-weeks_zscores.csv"
+
+# print(SOURCE_DATA_FILE)
+
+df = pd.read_csv(SOURCE_DATA_FILE)
+print(df.shape)
+print(df.columns)
